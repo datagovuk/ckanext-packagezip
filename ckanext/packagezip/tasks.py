66f3a9ebe9542238275167d760633a127bf0b19e
Tom Mortimer-Jones <tom@morty.co.uk>
2015-07-29 10:11:10 +0100

    Add index.html and datapackage.json to zip file

--- ckanext/packagezip/tasks.py
+++ ckanext/packagezip/tasks.py
@@ -6,6 +6,9 @@ from pylons import config
 
 import os
 import zipfile
+import tempfile
+import json
+import jinja2
 
 @celery.task(name="packagezip.create_zip")
 def create_zip(ckan_ini_filepath, package_id, queue='bulk'):
@@ -16,8 +19,30 @@ def create_zip(ckan_ini_filepath, package_id, queue='bulk'):
     filename = "{0}.zip".format(package_id)
     filepath = os.path.join(directory, filename)
     with zipfile.ZipFile(filepath, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        datapackage = {
+            'name': pkg['name'],
+            'title': pkg['title'],
+            'resources': [],
+        }
+
         for res in pkg['resources']:
            archival = Archival.get_for_resource(res['id'])
            if archival and archival.cache_filepath:
                path, resource_id, filename = archival.cache_filepath.rsplit('/', 2)
-               zipf.write(archival.cache_filepath, filename)
+               zipf.write(archival.cache_filepath, 'resources/{0}'.format(filename))
+
+               datapackage['resources'].append({'path': 'resources/{0}'.format(filename),
+                                                'description': res['description'],
+                                                'url': res['url']})
+
+        template = jinja2.Template('''<html>
+                                        <h1>{{datapackage.title}}</h1>
+                                        <ul>
+                                        {% for resource in datapackage.resources %}
+                                          <li><a href="{{resource.path}}">{{resource.description}}</a></li>
+                                        {% endfor %}
+                                        </ul>
+                                      </html>''')
+
+        zipf.writestr('index.html', template.render(datapackage=datapackage))
+        zipf.writestr('datapackage.json', json.dumps(datapackage, indent=4))
--- tests/test_package_zip.py
+++ tests/test_package_zip.py
@@ -9,8 +9,13 @@ import os
 import zipfile
 import uuid
 import json
+import random
+
+from lxml.html import fromstring
 
 from pylons import config
+
+words = ['Ant', 'Bird', 'Cat', 'Dog', 'Elephant', 'Frog', 'Goat', 'Hippo']
 
 class TestPackageZip(BaseCase):
     @classmethod
@@ -22,13 +27,14 @@ class TestPackageZip(BaseCase):
         cls.config = config.__file__
 
     def setup(self):
-        self.pkg = self._test_package('http://example.com/index.html')
+        self.pkg = self._test_package('http://data.gov.uk/robots.txt')
 
     def _test_package(self, url, format=None):
         context = {'model': model, 'ignore_auth': True, 'session': model.Session, 'user': 'test'}
-        name = str(uuid.uuid4())
-        pkg = {'name': name, 'resources': [
-            {'url': url, 'format': format or 'TXT', 'description': 'Test'}
+        title = " ".join(random.sample(words, 3))
+        name = title.lower().replace(' ', '-') + '-' + str(uuid.uuid4())
+        pkg = {'title': title, 'name': name, 'resources': [
+            {'url': url, 'format': format or 'TXT', 'description': 'Robots.txt'}
             ]}
         pkg = get_action('package_create')(context, pkg)
 
@@ -36,6 +42,18 @@ class TestPackageZip(BaseCase):
             update(self.config, res['id']) # This causes test to depend on internet
 
         return pkg
+
+    def _create_zip_file(self, package_id):
+        from ckanext.packagezip.tasks import create_zip
+        # celery.send_task doesn't respect CELERY_ALWAYS_EAGER
+        res = create_zip.apply_async(args=['CCC', package_id, 'queue1'])
+        res.get()
+
+        dest_dir = config.get('ckanext.packagezip.destination_dir')
+        filename = "{0}.zip".format(package_id)
+        filepath = os.path.join(dest_dir, filename)
+
+        return filepath
 
     @mock.patch('ckan.lib.celery_app.celery.send_task')
     def test_package_archive_event_causes_create_zip_task(self, send_task):
@@ -72,17 +90,45 @@ class TestPackageZip(BaseCase):
     def test_create_zip_task(self):
         package_id = self.pkg['id']
 
-        from ckanext.packagezip.tasks import create_zip
-        # celery.send_task doesn't respect CELERY_ALWAYS_EAGER
-        res = create_zip.apply_async(args=['CCC', package_id, 'queue1'])
-        res.get()
+        filepath = self._create_zip_file(package_id)
 
-        dest_dir = config.get('ckanext.packagezip.destination_dir')
-        filename = "{0}.zip".format(package_id)
-        filepath = os.path.join(dest_dir, filename)
+        assert os.path.exists(filepath), filepath
+
+        zipf = zipfile.ZipFile(filepath, 'r')
+
+        assert zipf.getinfo('resources/robots.txt').compress_type == zipfile.ZIP_DEFLATED
+
+    def test_zip_index_file(self):
+        package_id = self.pkg['id']
+
+        filepath = self._create_zip_file(package_id)
 
         assert os.path.exists(filepath), filepath
 
         zipf = zipfile.ZipFile(filepath, 'r')
 
         assert zipf.getinfo('index.html').compress_type == zipfile.ZIP_DEFLATED
+
+        doc = fromstring(zipf.read('index.html'))
+
+        assert [self.pkg['title']] == doc.xpath('//h1/text()')
+        assert doc.xpath('//ul/li/a/@href') == ['resources/robots.txt']
+        assert doc.xpath('//ul/li/a/text()') == ['Robots.txt']
+
+    def test_zip_datapackage_file(self):
+        package_id = self.pkg['id']
+
+        filepath = self._create_zip_file(package_id)
+
+        assert os.path.exists(filepath), filepath
+
+        zipf = zipfile.ZipFile(filepath, 'r')
+
+        assert zipf.getinfo('datapackage.json').compress_type == zipfile.ZIP_DEFLATED
+        datapackage = json.loads(zipf.read('datapackage.json'))
+
+        assert datapackage['name'] == self.pkg['name']
+        assert datapackage['title'] == self.pkg['title']
+        assert len(datapackage['resources']) == 1
+        assert datapackage['resources'][0]['path'] == 'resources/robots.txt'
+        assert datapackage['resources'][0]['url'] == 'http://data.gov.uk/robots.txt'
