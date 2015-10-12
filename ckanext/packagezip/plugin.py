0c460c95e935311814724f84f3fe44401a212980
Tom Mortimer-Jones <tom@morty.co.uk>
2015-10-12 11:36:35 +0100

    Typo

--- ckanext/packagezip/plugin.py
+++ ckanext/packagezip/plugin.py
@@ -42,7 +42,7 @@ class PackageZipPlugin(p.SingletonPlugin):
         )
         map.connect(
             'zipfile',
-            '/dataset/{name}/datepackage.zip',
+            '/dataset/{name}/datapackage.zip',
             controller=controller,
             action='zipfile'
         )
